db5826eac3397c40d975921f0e055c2f78a17ecf
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2021-07-22 12:24:08 +0300

    Setup tests

--- jest-setup.js
+++ jest-setup.js
@@ -1,1 +1,7 @@
 require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
+
+import 'react-native-gesture-handler/jestSetup';
+
+// As of react-native@0.64.X file has moved
+// eslint-disable-next-line no-undef
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
